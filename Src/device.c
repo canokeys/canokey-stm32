cac5ed4d80d439499e1e26c12d2dfb93c7fb47fd
z4yx <z4yx@users.noreply.github.com>
2019-10-16 22:46:56 +0800

    upgrade core

--- Src/device.c
+++ Src/device.c
@@ -1,5 +1,16 @@
 #include "main.h"
+#include <device.h>
 
 void device_delay(int ms) { HAL_Delay(ms); }
 
 uint32_t device_get_tick(void) { return HAL_GetTick(); }
+
+uint8_t is_nfc(void) { return 0; }
+
+void device_disable_irq(void) { __disable_irq(); }
+
+void device_enable_irq(void) { __enable_irq(); }
+
+void device_start_blinking(uint8_t sec) {}
+
+void device_stop_blinking(void) {}
