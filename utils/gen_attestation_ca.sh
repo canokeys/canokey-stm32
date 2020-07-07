2b31fd119a03a44378bf63070e14e8de28b06ede
z4yx <z4yx@users.noreply.github.com>
2020-07-07 13:49:51 +0800

    scripts to generate CA of attestation cert

---
+++ utils/gen_attestation_ca.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+openssl genrsa -out ca.key 2048
+openssl req -config ./attestation-ca-cert.cnf -extensions ca_extensions_sec -x509 -days 7120 -new -key ca.key -nodes -out ca.pem
