c04d0d12a2a7a7eff6b432eacda210140effe226
z4yx <z4yx@users.noreply.github.com>
2020-07-07 21:53:40 +0800

    rm ca.srl from Git repo

--- utils/gen_attestation_ca.sh
+++ utils/gen_attestation_ca.sh
@@ -2,3 +2,4 @@
 set -e
 openssl genrsa -out ca.key 2048
 openssl req -config ./attestation-ca-cert.cnf -extensions ca_extensions_sec -x509 -days 7120 -new -key ca.key -nodes -out ca.pem
+echo 01 >ca.srl # create the serial file
